927bb17379b508854623ca4b1264b0c9c5a0c50f
Varga Szilárd <szilard@niif.hu>
2017-10-06 08:30:39 +0000

    Redirect bugfix, jquery-ui.min.css not found fix

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,4 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-$app->get   ('/', function($request, $response)  {  return $response->withRedirect($this->router->pathFor('home.index')); })->setName('home');
-
-$app->get   ('/home', 'HomeAction:index' )->setName('home.index');
+$app->get   ('/', 'HomeAction:index' )->setName('home');
